da8e3c572b690181131ed22fb200bd5f8c751512
Daniil Savchuk <dsavchuk93@yandex.ru>
2018-11-15 01:11:16 +0300

    Fixed imports

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='plrc_project',
-      version='0.1',
-      description='PLRC project',
-      packages=['plrc_project.Entities', 'plrc_project.ImageProcessing', 'plrc_project.Run', 'plrc_project.Utils',
-                'plrc_project'],
-      zip_safe=False)
